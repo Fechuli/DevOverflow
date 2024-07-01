dd5b791ea802493a9e8b026a9508caaf5b400184
Federico Fiaschi <federicofiaschi277@gmail.com>
2024-07-01 14:40:16 +0200

    filters

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,4 +1,8 @@
+import HomeFilters from "@/components/home/HomeFilters";
+import Filter from "@/components/shared/Filter";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
 import { Button } from "@/components/ui/button";
+import { HomePageFilters } from "@/constants/filters";
 import Link from "next/link";
 
 export default function Home() {
@@ -11,11 +15,18 @@ export default function Home() {
           <Button className="primary-gradient min-h-[46px] px-4 py-3 !text-light-900">Ask a question</Button>
         </Link>
       </div>
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        LocalSearchBar 
+      <div className="mt-11 flex-col justify-between gap-5 max-sm:flex sm:items-center">
+        <LocalSearchbar route="/" iconPosition="left" imgSrc="/assets/icons/search.svg" placeholder="Search for questions" otherClasses="flex-1"/>
+        <Filter 
+          filters={HomePageFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+          containerClasses="hidden max-md:flex"
+        /> 
 
-        Filters 
       </div>
+
+      <HomeFilters />
+
     </>
   );
 }
