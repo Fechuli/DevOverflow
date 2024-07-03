6670c281f9fd3e050bf55a90d972ef0d4cda450b
Federico Fiaschi <federicofiaschi277@gmail.com>
2024-07-03 14:41:21 +0200

    stable versione of clerk

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,22 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
-
-// const isProtectedRoute = createRouteMatcher(["/ask-question(.*)"]);
-const publicRoutes = [
-  '/',
-  '/api/webhook',
-  '/question/:id',
-  '/tags',
-  '/tags/:id',
-  '/profile/:id',
-  '/community',
-  '/jobs'
-]
-const ignoredRoutes = [
-  '/api/webhook',
-  '/api/chatgpt'
-]
-
-const isPublicRoute = createRouteMatcher(publicRoutes);
-const isIgnoredRoute = createRouteMatcher(ignoredRoutes);
-
-export default clerkMiddleware((auth, req) => {
-  if (isIgnoredRoute(req)) return;
-  if (!isPublicRoute(req)) {
-    auth().protect();
-  }
+import { authMiddleware } from "@clerk/nextjs";
+ 
+export default authMiddleware({
+  publicRoutes: [
+    '/',
+    '/api/webhook',
+    '/question/:id',
+    '/tags',
+    '/tags/:id',
+    '/profile/:id',
+    '/community',
+    '/jobs'
+  ],
+  ignoredRoutes: [
+    '/api/webhook', '/api/chatgpt'
+  ]
 });
-
+ 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
+ 
