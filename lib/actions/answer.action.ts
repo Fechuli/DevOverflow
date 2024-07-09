149f95342c518d5b6cbca8c3007ab6612eb169f6
Federico Fiaschi <federicofiaschi277@gmail.com>
2024-07-09 15:57:02 +0200

    edit and delte answer question

--- lib/actions/answer.action.ts
+++ lib/actions/answer.action.ts
@@ -2,9 +2,10 @@
 
 import Question from "@/database/question.model";
 import { connectToDatabase } from "../mongoose";
-import { AnswerVoteParams, CreateAnswerParams, GetAnswersParams } from "./shared.types";
+import { AnswerVoteParams, CreateAnswerParams, DeleteAnswerParams, GetAnswersParams } from "./shared.types";
 import Answer from "@/database/answer.model";
 import { revalidatePath } from "next/cache";
+import Interaction from "@/database/interaction.model";
 
 export async function createAnswer(params: CreateAnswerParams) {
   try {
@@ -95,6 +96,29 @@ export async function downvoteAnswer(params: AnswerVoteParams) {
 
     revalidatePath(path);
 
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteAnswer(params: DeleteAnswerParams) {
+  try {
+    connectToDatabase();
+    const { answerId, path } = params;
+
+    const answer = await Answer.findById(answerId);
+    if(!answer) {
+      throw new Error("Answer not found");
+    }
+
+    await answer.deleteOne({_id: answerId});
+    await Question.updateMany({_id: answer.question}, {$pull: {answers: answerId}})
+    await Interaction.deleteMany({answer: answerId});
+
+
+    revalidatePath(path);
+    
   } catch (error) {
     console.log(error);
     throw error;
